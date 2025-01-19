8860de7de984e223578a48e552d930abf8a3391c
abram7777777 <abramashraf28@gmail.com>
2025-01-19 21:41:34 +0200

    layout update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Home from './componants/home/Home'
 import About from './componants/about/About'
 import Protofoilo from './componants/protofolio/Protofoilo'
 import Contact from './componants/contact/Contact'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Mainlayout from './componants/MainLayout/MainLayout'
 import Notfound from './componants/notfound/Notfound'
 import 'flowbite-react'
 import 'flowbite'
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import Layout from './componants/Layout/Layout';
 
 
 function App() {
@@ -19,7 +17,7 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "",
-      element:<Mainlayout/>,
+      element:<Layout/>,
       children:[
         {index:true , element:<Home/>},
         {path:"about" , element:<About/>},
